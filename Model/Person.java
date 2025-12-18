00f26281a926d302813e0b7a92a2be5fc8b32ede
Mohamad  Khir Allah <mohamadka@Mohamads-MacBook-Air-7.local>
2025-12-18 16:33:30 +0000

    Added Person class as a base class to store and manage personal information for entities like Patients and dtuff

---
+++ Model/Person.java
@@ -0,0 +1,60 @@
+package Model;
+
+public abstract class Person {
+    protected String user_id;
+    protected String first_name;
+    protected String last_name;
+    protected String phone_number;
+    protected String email;
+    protected Date date_of_birth;
+
+
+    public Person(String user_id, String first_name, String last_name, String phone_number, String email, Date date_of_birth) {
+        this.user_id = user_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_number = phone_number;
+        this.email = email;
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
